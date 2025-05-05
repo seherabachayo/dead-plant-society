4a192462f01a07ec1c03034401757d58f73f1e61
maddie <mads8956@gmail.com>
2025-05-04 18:59:14 -0700

    Set up schema and began configuring APIs

---
+++ models/users.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true
+    },
+    password:
+    {
+        type: String,
+        required: true
+    },
+   
+
+    //timestamps : true to keep track of when doc was created
+});
+
+const User = mongoose.model('User', userSchema); //create a collection called User using this schema
+//note: mongooose will take User and make users collection
+export default User;
