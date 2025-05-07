1a73787154c266046db7f49bb9abf137da8ab521
maddie <mads8956@gmail.com>
2025-05-06 19:47:15 -0700

    working on APIs

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,23 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 
 dotenv.config();
-
 const app = express();
-
-//server get request
-app. get("/users", (req, res) => {});
-
-
-
-
-app.listen(5000, () =>{ 
-    connectDB(); // connect database before starting server
-    console.log('Server is at http://localhost:5000/');
-});
+ 
+ //server get request
+ app. get("/users", (req, res) => {});
+ app.post("/users", async (req, res) => {
+     const user = req.body; // client will send this data
+     if(!user.username || !user.password){
+         return res.status(400).json({ success:false, message: "Please enter username and password"});
+     }
+     
+     //User=from our product model users.js, user=from client
+     const newUser = new User(user);
+     try{
+         await newUser.save()//saves to our database
+         res.status(201).json({success: true, data: newUser});//201 when success and something was created
+     } catch(error){
+         console.error("Error in creating user", error.message);
+         res.status(500).json({success: false, message: "Sever Error"});
+     }
+ });
