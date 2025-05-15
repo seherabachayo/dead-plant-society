2cd7c49e547ce768d1c6a6227a591bbb028f2a2d
maddie <mads8956@gmail.com>
2025-05-14 20:10:49 -0700

    added API functions and organized file structure

--- server/server.js
+++ server/server.js
@@ -2,46 +2,16 @@ import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import User from './models/users.js';//import schema
+import mongoose from 'mongoose';
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 const app = express();
  app.use(express.json());//allows server to accept json data in req body
 
+app.use("/api/users", userRoutes);//calls methods in user.route.js
 
- //server get request
- //app. get("/users", (req, res) => {});
- app.post("/api/users", async (req, res) => {
-     const user = req.body; // client will send this data
-     if(!user.username || !user.password){
-         return res.status(400).json({ success:false, message: "Please enter username and password"});
-     }
-     
-     //User=from our product model users.js, user=from client
-     const newUser = new User(user);
-     try{
-         await newUser.save()//saves to our database
-         res.status(201).json({success: true, data: newUser});//201 when success and something was created
-     } catch(error){
-         console.error("Error in creating user", error.message);
-         res.status(500).json({success: false, message: "Sever Error"});
-     }
- });
-
- //to delete users in database
- app.delete("/api/users/:id", async (req, res)=> {
-    const{id} = req.params;
-    console.log("id:", id);
-
-    try{
-        await User.findByIdAndDelete(id);
-        res.status(200).json({success: true, message: "User removed"});
-    }
-    catch(error) {
-        res.status(404).json({sucess: false, message: "User not found"})
-    }
- });
-
-
+ //start server
 app.listen(5000, () => {
 	connectDB();
 	console.log("Server started at http://localhost:5000");
