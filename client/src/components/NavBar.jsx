df339308dec0e7d3b6590fc2f7da941826fc1d91
Shane Song <ssong17@g.ucla.edu>
2025-05-22 18:09:23 -0700

    Just changed some typos to make things run right

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/create">New Post</Link></li>
-        <li><Link to="/expandedpost">Expanded Post</Link></li>
+        <li><Link to="/expanded-post">Expanded Post</Link></li>
         <li><Link to="/login">Login</Link></li>
         <li><Link to="/logform">Log Form</Link></li>
       </ul>
