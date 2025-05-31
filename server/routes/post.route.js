4c8ed453f6d3517c398462a495ae664f6a8a4176
maddie <mads8956@gmail.com>
2025-05-30 17:15:21 -0700

    added get posts and search post captions APIs

--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
-import{createPost} from "../controllers/post.controller.js"
+import{createPost, getPosts, searchPosts} from "../controllers/post.controller.js"
 
 
  router.post("/", createPost);
-
+ router.get("/", getPosts);
+ router.get('/search', searchPosts);
 
 export default router;
