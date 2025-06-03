3d48c0fb44233804dcf030f3a2dbdfe9cdc386fc
Shane Song <ssong17@g.ucla.edu>
2025-06-02 19:16:04 -0700

    Added to Comment.jsx by extracting user to display profile pic and username

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,14 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Comment.css'; 
 
 const Comment = ({ values }) => {
   const [likes, setLikes] = useState(0);
+  const [poster,setPoster]=useState({});
+  useEffect(() => {
+          fetch(
+              `http://localhost:5050/api/users/${values.poster}` //fetches poster with same id as in url
+          ).then(res => {
+              if(!res.ok) throw new Error(res.statusText);
+              return res.json(); 
+          })
+          .then(
+              data=>{
+                  if(data.success && data.data){
+                      setPoster(data.data); 
+                  }
+                  else{
+                  throw new Error('Invalid format');
+                  }
+              })
+              .catch(err => {
+              setError(err.message);
+          })
+        .finally(() => {
+          setLoading(false);
+        });
+  })
 
   return (
     <div className="comment">
-      <h1 className="username">
-        <img className="profile_pic" src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQddho8dHAH10wgORZ8jw_2gIBRxWNdKtTo5Q&s"} alt={`${values.name}'s profile`} />
-        {values.name}
+      <h1 className="username">            {/*user profile pic*/}
+        <img className="profile_pic" src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQddho8dHAH10wgORZ8jw_2gIBRxWNdKtTo5Q&s"} alt={`${poster.username}'s profile`} />
+        {poster.username}
       </h1> 
       
       <h2 className="comment_text">{values.content}</h2>
