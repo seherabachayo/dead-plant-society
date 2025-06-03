ed551d34af8aef53ec3093809aaee020526eb1a8
JavierCastaneda95 <zelda86jc@gmail.com>
2025-06-02 18:38:39 -0700

    Fixed a backend issue, changing getThisPost to find post through id

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -64,12 +64,12 @@ export const createPost = async (req, res) => {
      
          try {
         //find the user by id
-        const post = await User.findById(id);
+        const post = await Post.findById(id);
         if (!post) {
           return res.status(404).json({ success: false, message: "Post not found" });
         }
     
-        return res.status(200).json({ success: true, data: user });
+        return res.status(200).json({ success: true, data: post });
         } 
         catch (error) {
             return res.status(500).json({ success: false, message: "Server Error" });
